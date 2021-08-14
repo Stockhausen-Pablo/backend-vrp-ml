10f1a1cb16e511bec90b5e44635ea82c95ba4a5a
Pablo Stockhausen <stockhausen017@gmail.com>
2021-08-14 11:18:55 +0200

    testing adjustments

--- main.py
+++ main.py
@@ -70,169 +70,6 @@ def load_global_parameters():
     parameter_groups = jsonObject
 
 
-def main(args):
-    # --------------------
-    # ARGSPARSE
-    # define parameters (the wall of parameters)
-
-    # TRAINING
-    num_episodes = args['num_episodes']
-    max_steps = args['max_steps']
-
-    # ML-PARAMETER
-    learning_rate = args['learning_rate']
-    stay_duration = args['stay_duration']
-    discount_factor = args['discount_factor']
-    exploration_factor = args['exploration_factor']
-    ml_agent = args['agent']
-    increasing_factor = args['increasing_factor']
-    increasing_factor_good_episode = args['increasing_factor_good_episode']
-    decreasing_factor = args['decreasing_factor']
-    decreasing_factor_good_episode = args['decreasing_factor_good_episode']
-    baseline_theta = args['baseline_theta']
-
-    # THRESHOLD PARAMETERS
-    # see argsConfig for help
-    distance_utilization_threshold = args['distance_utilization_threshold']
-    capacity_utilization_threshold = args['capacity_utilization_threshold']
-    local_search_threshold = args['local_search_threshold']
-    policy_reset_threshold = args['policy_reset_threshold']
-
-    # ACO-PARAMETER
-    aco_alpha_factor = args['aco_alpha_factor']
-    aco_beta_factor = args['aco_beta_factor']
-    pheromone_evaporation_coefficient = args['pheromone_evaporation_coefficient']
-    pheromone_constant = args['pheromone_constant']
-    aco_iterations = args['aco_iterations']
-    aco_increasing_factor = args['aco_increasing_factor']
-
-    # --------------------
-    # INPUT
-    # define meta data
-    print("---------System menu---------")
-    print("Below please specify the configuration options of the program")
-    data_input = input("Please specify the data source of the stops to be processed:") or 'short_train_data'
-    print('-Regarding the Microhub name, this should be unique and used only for this Microhub.-')
-    print('-The model of the agent is saved but also loaded based on the microhub names.-')
-    microhub_name = input("Please specify the microhub name:") or "TestHub"
-    shipper_name = input("Please specify the shipper name:") or "TestVersender"
-    carrier_name = input("Please specify the carrier name:") or "TestCarrier"
-    print('-Enter the delivery date. Possible Answers [Mon, Tue, Wed, Thurs, Fri, Sat]')
-    delivery_date = input("Please specify the delivery date:") or "Test"
-    amount_vehicles = int(input("How many vehicles will be used:") or 2)
-    vehicle_speed = int(input("How fast is the vehicle [km/h]: ") or 30)
-    capacity_weight = float(input("What is the maximum weight that the vehicle can carry:") or 180)
-    capacity_volume = float(input("What is the maximum volume that the vehicle can hold:") or 500)
-
-    # --------------------
-    # SETTING UP TOUR MANAGER
-    # Load Stop Data
-    tManager.clear()
-    load_stop_data(data_input)
-
-    # Setup Distance Matrix for later use
-    distance_matrix = tManager.get_distances()
-
-    # --------------------
-    # PLOT COORDINATES
-    # overview of problem space (input)
-    plot_coordinates_with_coordinates_as_label()
-    plot_coordinates_with_stopnr__as_label()
-
-    if args['test']:
-        # --------------------TESTING MODE--------------------
-        print("-Entered Testing Mode-")
-        testing_start = timer()
-        # --------------------
-        # ENVIRONMENT
-        # setting up MDP-Environment
-        environment = VRPEnvironment(
-            states=tManager.get_list_of_stops(),
-            # actions:
-            # 0 = select microhub if tour full and possible Stops != null
-            # 1 = select unvisited Node from possible Stops
-            # 2 = select microhub if tour full and possible Stops = null
-            actions=[0, 1, 2],
-            distance_matrix=distance_matrix,
-            microhub=tManager.get_microhub(),
-            capacity_demands=tManager.get_capacity_demands_as_dict(),
-            vehicles=amount_vehicles,
-            vehicle_weight=capacity_weight,
-            vehicle_volume=capacity_volume
-        )
-
-        # --------------------
-        # POLICY NETWORK
-        policyManager = PolicyManager(environment.get_all_state_hashes(),
-                                      learning_rate,
-                                      discount_factor,
-                                      exploration_factor,
-                                      increasing_factor,
-                                      increasing_factor_good_episode,
-                                      decreasing_factor,
-                                      decreasing_factor_good_episode,
-                                      baseline_theta,
-                                      distance_utilization_threshold,
-                                      capacity_utilization_threshold,
-                                      local_search_threshold,
-                                      policy_reset_threshold
-                                      )
-
-        # --------------------
-        # LOAD PREVIOUS ML-MODEL
-        model_name = create_model_name(microhub_name, capacity_weight, capacity_volume, shipper_name, carrier_name,
-                                       delivery_date, ml_agent)
-        policyManager.loadModel(model_name)
-
-        # --------------------
-        # CONSTRUCTION SOLUTION
-        current_policy_reward, final_tours = policyManager.construct_policy(policyManager.get_current_policy(),
-                                                                            environment, max_steps)
-        testing_end = timer()
-
-        for tour in final_tours:
-            for stop in tour:
-                print(stop.tourStopId)
-
-        # --------------------
-        # CALCULATE/PRINT META TOUR CONSTRUCTION DATA
-        total_box_amount = 0
-        total_weight = 0.0
-        total_volume = 0.0
-
-        for tour in final_tours:
-            for stop in tour:
-                total_box_amount += stop.box_amount
-                total_weight += stop.demand_weight
-                total_volume += stop.demand_volume
-
-        mean_box_amount = total_box_amount / len(final_tours)
-        mean_volume = total_volume / len(final_tours)
-        mean_weight = total_weight / len(final_tours)
-
-        # total_time, total_distance, average_time_per_tour = calculate_delivery_time(vehicle_speed, stay_duration, final_tours)
-        total_time, total_distance, average_time_per_tour, average_distance_per_tour = calculate_tour_meta(
-            vehicle_speed, stay_duration, final_tours)
-
-        print("Stop Amount: ", len(tManager.get_list_of_stops()))
-        print("TESTING RUN TIME in s: ", (testing_end - testing_start))
-        print("Amount of constructed Tours: ", len(final_tours))
-        print("Mean Box Amount per Tour: ", mean_box_amount)
-        print("Mean Volume per Tour: ", mean_volume)
-        print("Mean Weight per Tour: ", mean_weight)
-        print("Lost Volume per Tour: ", (capacity_volume - mean_volume))
-        print("Lost Weight per Tour: ", (capacity_weight - mean_weight))
-        print("Overall distance: ", total_distance)
-        print("Mean Distance per Tour: ", (total_distance / len(final_tours)))
-        print("Overall Time needed: ", total_time)
-        print("Average Time needed per Tour: ", average_time_per_tour)
-
-        # --------------------
-        # PLOTTING TOUR (UNNECESSARY IN PRODUCTION)
-        plot_tour_with_stopnr_as_label(final_tours)
-        plot_tours_individual(final_tours, model_name, True)
-
-
 def start_server(args):
     global parameter_groups
     # --------------------
@@ -452,11 +289,141 @@ def start_server(args):
         """
         policyManager.saveModel(model_name)
 
-        return {'POST': "successful"}, 200
+        return {'POST': "Training successful"}, 200
 
-    @app.route('/ml-service/testing', methods=['GET'])
+    @app.route('/ml-service/testing/start', methods=['GET'])
     def ml_service_testing():
-        return "<h1>Distant Reading Archive</h1><p>Home</p>"
+        # --------------------TRAINING MODE--------------------
+        print("-Entered Training Mode-")
+        global parameter_groups
+        testing_start = timer()
+        # --------------------
+        # ENVIRONMENT
+        # setting up MDP-Environment
+        environment = VRPEnvironment(
+            states=tManager.get_list_of_stops(),
+            actions=[0, 1, 2],
+            distance_matrix=distance_matrix,
+            microhub=tManager.get_microhub(),
+            capacity_demands=tManager.get_capacity_demands_as_dict(),
+            vehicles=parameter_groups['groups'][0]['amount_vehicles'],
+            vehicle_weight=parameter_groups['groups'][0]['capacity_weight'],
+            vehicle_volume=parameter_groups['groups'][0]['capacity_volume']
+        )
+
+        # --------------------
+        # POLICY NETWORK
+        policyManager = PolicyManager(environment.get_all_state_hashes(),
+                                      parameter_groups['groups'][2]['learning_rate'],
+                                      parameter_groups['groups'][2]['discount_factor'],
+                                      parameter_groups['groups'][2]['exploration_factor'],
+                                      parameter_groups['groups'][3]['increasing_factor'],
+                                      parameter_groups['groups'][3]['increasing_factor_good_episode'],
+                                      parameter_groups['groups'][3]['decreasing_factor'],
+                                      parameter_groups['groups'][3]['decreasing_factor_good_episode'],
+                                      parameter_groups['groups'][2]['baseline_theta'],
+                                      parameter_groups['groups'][5]['distance_utilization_threshold'],
+                                      parameter_groups['groups'][5]['capacity_utilization_threshold'],
+                                      parameter_groups['groups'][5]['local_search_threshold'],
+                                      parameter_groups['groups'][5]['policy_reset_threshold']
+                                      )
+
+        # --------------------
+        # LOAD PREVIOUS ML-MODEL
+        model_name = create_model_name(parameter_groups['groups'][0]['microhub_name'],
+                                       parameter_groups['groups'][0]['capacity_weight'],
+                                       parameter_groups['groups'][0]['capacity_volume'],
+                                       parameter_groups['groups'][0]['shipper_name'],
+                                       parameter_groups['groups'][0]['carrier_name'],
+                                       parameter_groups['groups'][0]['delivery_date'],
+                                       parameter_groups['groups'][2]['ml_agent'])
+        policyManager.loadModel(model_name)
+
+        # --------------------
+        # CONSTRUCTION SOLUTION
+        current_policy_reward, final_tours = policyManager.construct_policy(policyManager.get_current_policy(),
+                                                                            environment, parameter_groups['groups'][1]['max_steps'])
+        testing_end = timer()
+
+        for tour in final_tours:
+            for stop in tour:
+                print(stop.tourStopId)
+
+        # --------------------
+        # CALCULATE/PRINT META TOUR CONSTRUCTION DATA
+        total_box_amount = 0
+        total_weight = 0.0
+        total_volume = 0.0
+
+        for tour in final_tours:
+            for stop in tour:
+                total_box_amount += stop.box_amount
+                total_weight += stop.demand_weight
+                total_volume += stop.demand_volume
+
+        mean_box_amount = total_box_amount / len(final_tours)
+        mean_volume = total_volume / len(final_tours)
+        mean_weight = total_weight / len(final_tours)
+
+        # total_time, total_distance, average_time_per_tour = calculate_delivery_time(vehicle_speed, stay_duration, final_tours)
+        total_time, total_distance, average_time_per_tour, average_distance_per_tour = calculate_tour_meta(
+            parameter_groups['groups'][0]['vehicle_speed'], parameter_groups['groups'][0]['stay_duration'], final_tours)
+
+        stop_amount = len(tManager.get_list_of_stops())
+        testing_runtime = testing_end - testing_start
+        amount_constructed_tours = len(final_tours)
+        mean_box_amount_per_tour = mean_box_amount
+        mean_volume_per_tour = mean_volume
+        mean_weight_per_tour = mean_weight
+        lost_volume_per_tour = float(parameter_groups['groups'][0]['capacity_volume']) - mean_volume
+        lost_weight_per_tour = float(parameter_groups['groups'][0]['capacity_weight']) - mean_weight
+        overall_distance = total_distance
+        mean_distance_per_tour = total_distance / len(final_tours)
+        overall_time_needed = total_time
+        average_time_needed_per_tour = average_time_per_tour
+
+        json_string = json.dumps({
+            "stop_amount": stop_amount,
+            "testing_runtime": testing_runtime,
+            "amount_constructed_tours": amount_constructed_tours,
+            "mean_box_amount_per_tour": mean_box_amount_per_tour,
+            "mean_volume_per_tour": mean_volume_per_tour,
+            "mean_weight_per_tour": mean_weight_per_tour,
+            'lost_volume_per_tour': lost_volume_per_tour,
+            'lost_weight_per_tour': lost_weight_per_tour,
+            'overall_distance': overall_distance,
+            'mean_distance_per_tour': mean_distance_per_tour,
+            'overall_time_needed': overall_time_needed,
+            'average_time_needed_per_tour': average_time_needed_per_tour
+        })
+
+        # --------------------
+        # PLOTTING TOUR (UNNECESSARY IN PRODUCTION)
+        plot_tour_with_stopnr_as_label(final_tours)
+        plt1 = plot_tours_individual(final_tours, model_name, True)
+        img1 = BytesIO()
+        plt1.savefig(img1, format='png', bbox_inches='tight')
+        img1.seek(0)
+        plot1_url = base64.b64encode(img1.getvalue()).decode()
+        plt1.close()
+
+        json_string = json.dumps({
+            "stop_amount": stop_amount,
+            "testing_runtime": testing_runtime,
+            "amount_constructed_tours": amount_constructed_tours,
+            "mean_box_amount_per_tour": mean_box_amount_per_tour,
+            "mean_volume_per_tour": mean_volume_per_tour,
+            "mean_weight_per_tour": mean_weight_per_tour,
+            'lost_volume_per_tour': lost_volume_per_tour,
+            'lost_weight_per_tour': lost_weight_per_tour,
+            'overall_distance': overall_distance,
+            'mean_distance_per_tour': mean_distance_per_tour,
+            'overall_time_needed': overall_time_needed,
+            'average_time_needed_per_tour': average_time_needed_per_tour,
+            'final_tours_base64': plot1_url
+        })
+
+        return json_string, 200
 
     app.debug = True
     app.run(threaded=True)
